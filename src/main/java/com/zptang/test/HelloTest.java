3cdf150306f7a9434dd629772fb3c554d4113ecb
zhangptang <1004676373@qq.com>
2019-07-29 22:55:11 +0800

    change it with input

--- src/main/java/com/zptang/test/HelloTest.java
+++ src/main/java/com/zptang/test/HelloTest.java
@@ -3,5 +3,6 @@ package com.zptang.test;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("this is the first start with git.I like it.");
     }
 }
